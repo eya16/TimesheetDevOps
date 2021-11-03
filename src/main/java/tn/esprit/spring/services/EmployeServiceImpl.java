5a6781595f273dfc5e8d5c4a5bc651788e49735a
Chedy Rhaiem <75956233+ChedyRh@users.noreply.github.com>
2021-11-03 21:26:55 +0100

    add Log4J_Junit_Employee

--- src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,15 +33,32 @@ public class EmployeServiceImpl implements IEmployeService {
 	@Autowired
 	TimesheetRepository timesheetRepository;
 
+	private static final Logger logger = Logger.getLogger(EmployeServiceImpl.class);
 	public int ajouterEmploye(Employe employe) {
+		try {
+		logger.debug("lancement de l'ajout  d'un employee ");
 		employeRepository.save(employe);
+		logger.info("l'ajout est terminé avec succés! ");
+		}catch(Exception e) {
+			logger.error("Erreur dans la methode ajouterEmploye():"+ e);
+		}finally {
+			logger.info("Methode ajouterEmploye est terminée");
+		}
+
 		return employe.getId();
 	}
 
 	public void mettreAjourEmailByEmployeId(String email, int employeId) {
+		try {
+			logger.debug("lancement de mise ajour de l'email d'un employee par id ");
 		Employe employe = employeRepository.findById(employeId).get();
 		employe.setEmail(email);
 		employeRepository.save(employe);
+		}catch(Exception e) {
+			logger.error("Erreur dans la methode mettreAjourEmailByEmployeId:"+ e);
+		}finally {
+			logger.info("Methode mettreAjourEmailByEmployeId est terminée sans erreur");
+		}
 
 	}
 
@@ -49,7 +68,7 @@ public class EmployeServiceImpl implements IEmployeService {
 		Employe employeManagedEntity = employeRepository.findById(employeId).get();
 
 		if(depManagedEntity.getEmployes() == null){
-
+			logger.info("verifier s'il y'a deja une liste d'employe et creation d'une nouvelle s'il n'ya pas");
 			List<Employe> employes = new ArrayList<>();
 			employes.add(employeManagedEntity);
 			depManagedEntity.setEmployes(employes);
@@ -58,16 +77,18 @@ public class EmployeServiceImpl implements IEmployeService {
 			depManagedEntity.getEmployes().add(employeManagedEntity);
 
 		}
-
+		logger.info("affecterEmployeADepartement finit avec succes");
 	}
 	@Transactional
 	public void desaffecterEmployeDuDepartement(int employeId, int depId)
 	{
+		logger.debug("essai de desaffecterEmployeDuDepartement");
 		Departement dep = deptRepoistory.findById(depId).get();
 
 		int employeNb = dep.getEmployes().size();
 		for(int index = 0; index < employeNb; index++){
 			if(dep.getEmployes().get(index).getId() == employeId){
+				logger.info("l'id fournit doit etre existante");
 				dep.getEmployes().remove(index);
 				break;//a revoir
 			}
@@ -75,27 +96,38 @@ public class EmployeServiceImpl implements IEmployeService {
 	}
 
 	public int ajouterContrat(Contrat contrat) {
+		try {
+			logger.debug("lancement de l'ajout  d'un contrat ");
 		contratRepoistory.save(contrat);
+		}catch(Exception e) {
+			logger.error("Erreur dans la methode ajouterContrat:"+ e);
+		}finally {
+			logger.info("Méthode ajouterContrat términé !");
+		}
 		return contrat.getReference();
 	}
 
 	public void affecterContratAEmploye(int contratId, int employeId) {
+		logger.debug("on est dans affecterContratAEmploye ");
 		Contrat contratManagedEntity = contratRepoistory.findById(contratId).get();
 		Employe employeManagedEntity = employeRepository.findById(employeId).get();
 
 		contratManagedEntity.setEmploye(employeManagedEntity);
 		contratRepoistory.save(contratManagedEntity);
-		
+		logger.info("affecterContratAEmploye términé !");
 	}
 
 	public String getEmployePrenomById(int employeId) {
+		logger.debug("lancement de getEmployePrenomById");
 		Employe employeManagedEntity = employeRepository.findById(employeId).get();
+		logger.info("selection de l'employe selon l'id et passage au retour de son nom ");
 		return employeManagedEntity.getPrenom();
 	}
 	public void deleteEmployeById(int employeId)
 	{
+		logger.debug("lancement de la supression de l'employe selon l'id");
 		Employe employe = employeRepository.findById(employeId).get();
-
+		logger.info("selection de l'employe selon l'id et apssage a l'etape d e suppression de tous les departement");
 		//Desaffecter l'employe de tous les departements
 		//c'est le bout master qui permet de mettre a jour
 		//la table d'association
@@ -107,46 +139,68 @@ public class EmployeServiceImpl implements IEmployeService {
 	}
 
 	public void deleteContratById(int contratId) {
+		logger.debug("lancement de la supression d'un contrat selon l'id");
 		Contrat contratManagedEntity = contratRepoistory.findById(contratId).get();
 		contratRepoistory.delete(contratManagedEntity);
 
 	}
 
 	public int getNombreEmployeJPQL() {
+		logger.debug("lancement de l'affichage de nombre des employes");
 		return employeRepository.countemp();
 	}
 	
 	public List<String> getAllEmployeNamesJPQL() {
+		logger.debug("lancement de l'affichage de la liste des noms des employes");
 		return employeRepository.employeNames();
 
 	}
 	
 	public List<Employe> getAllEmployeByEntreprise(Entreprise entreprise) {
-		return employeRepository.getAllEmployeByEntreprisec(entreprise);
+		List<Employe> employes=null;
+		try {
+			logger.debug("lancement de l'affichage de la liste des employes selon entreprise");
+		employes=employeRepository.getAllEmployeByEntreprisec(entreprise);
+		logger.info("liste des employes selon entreprise!");
+		}catch (Exception e) {
+			logger.error("Erreur dans la méthode   getAllEmployeByEntreprise(): "+ e);
+		}
+		return employes;
 	}
 
 	public void mettreAjourEmailByEmployeIdJPQL(String email, int employeId) {
+		logger.debug("nous somme dans mettreAjourEmailByEmployeIdJPQL");
 		employeRepository.mettreAjourEmailByEmployeIdJPQL(email, employeId);
 
 	}
 	public void deleteAllContratJPQL() {
+		logger.debug("lancement de deleteAllContratJPQL");
          employeRepository.deleteAllContratJPQL();
 	}
 	
 	public float getSalaireByEmployeIdJPQL(int employeId) {
+		logger.debug("lancement de getSalaireByEmployeIdJPQL");
 		return employeRepository.getSalaireByEmployeIdJPQL(employeId);
 	}
 
 	public Double getSalaireMoyenByDepartementId(int departementId) {
+		logger.debug("lancement de getSalaireMoyenByDepartementId");
 		return employeRepository.getSalaireMoyenByDepartementId(departementId);
 	}
 	
 	public List<Timesheet> getTimesheetsByMissionAndDate(Employe employe, Mission mission, Date dateDebut,
 			Date dateFin) {
-		return timesheetRepository.getTimesheetsByMissionAndDate(employe, mission, dateDebut, dateFin);
+		List<Timesheet> timesheets=null;
+		try {
+			logger.debug("lancement de l'affichage de la liste des timesheets avec missions et date");
+		 timesheets = timesheetRepository.getTimesheetsByMissionAndDate(employe, mission, dateDebut, dateFin);
+		 logger.info("liste des timesheet avec missions et date !!");
+		}catch(Exception e) {logger.error("Erreur dans la méthode  getTimesheetsByMissionAndDate(): "+ e);}
+		return timesheets;
 	}
 
 	public List<Employe> getAllEmployes() {
+		logger.debug("lancement de l'affichage de la liste des employes ");
 				return (List<Employe>) employeRepository.findAll();
 	}
 
