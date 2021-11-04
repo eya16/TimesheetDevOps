f05e8f4cd1bfde6f10ce3809b5259dfea079591a
eyaKsouri <eya.ksouri@esprit.tn>
2021-11-04 17:23:10 +0100

    add jacoco

--- src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/TimesheetServiceImpl.java
@@ -3,10 +3,12 @@ package tn.esprit.spring.services;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Employe;
@@ -21,7 +23,8 @@ import tn.esprit.spring.repository.TimesheetRepository;
 
 @Service
 public class TimesheetServiceImpl implements ITimesheetService {
-	
+	private static final Logger l = Logger.getLogger(TimesheetServiceImpl.class);
+
 
 	@Autowired
 	MissionRepository missionRepository;
@@ -38,11 +41,14 @@ public class TimesheetServiceImpl implements ITimesheetService {
 	}
     
 	public void affecterMissionADepartement(int missionId, int depId) {
-		Mission mission = missionRepository.findById(missionId).get();
-		Departement dep = deptRepoistory.findById(depId).get();
-		mission.setDepartement(dep);
-		missionRepository.save(mission);
+		Optional<Mission> mission = missionRepository.findById(missionId);
+		Optional<Departement> dep = deptRepoistory.findById(depId);
+
+		if(mission.isPresent() && dep.isPresent()) {
 		
+		mission.get().setDepartement(dep.get());
+		missionRepository.save(mission.get());
+		}
 	}
 
 	public void ajouterTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin) {
@@ -61,24 +67,27 @@ public class TimesheetServiceImpl implements ITimesheetService {
 
 	
 	public void validerTimesheet(int missionId, int employeId, Date dateDebut, Date dateFin, int validateurId) {
-		System.out.println("In valider Timesheet");
-		Employe validateur = employeRepository.findById(validateurId).get();
-		Mission mission = missionRepository.findById(missionId).get();
+		l.info("In valider Timesheet");
+		
+		Optional<Employe> validateur = employeRepository.findById(validateurId);
+		Optional<Mission> mission = missionRepository.findById(missionId);
+
+		if(validateur.isPresent() && mission.isPresent()) {
 		//verifier s'il est un chef de departement (interet des enum)
-		if(!validateur.getRole().equals(Role.CHEF_DEPARTEMENT)){
-			System.out.println("l'employe doit etre chef de departement pour valider une feuille de temps !");
+		if(!validateur.get().getRole().equals(Role.CHEF_DEPARTEMENT)){
+			l.info("l'employe doit etre chef de departement pour valider une feuille de temps !");
 			return;
 		}
 		//verifier s'il est le chef de departement de la mission en question
 		boolean chefDeLaMission = false;
-		for(Departement dep : validateur.getDepartements()){
-			if(dep.getId() == mission.getDepartement().getId()){
+		for(Departement dep : validateur.get().getDepartements()){
+			if(dep.getId() == mission.get().getDepartement().getId()){
 				chefDeLaMission = true;
 				break;
 			}
 		}
 		if(!chefDeLaMission){
-			System.out.println("l'employe doit etre chef de departement de la mission en question");
+			l.info("l'employe doit etre chef de departement de la mission en question");
 			return;
 		}
 //
@@ -88,11 +97,11 @@ public class TimesheetServiceImpl implements ITimesheetService {
 		
 		//Comment Lire une date de la base de données
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		System.out.println("dateDebut : " + dateFormat.format(timesheet.getTimesheetPK().getDateDebut()));
+		l.info("dateDebut : " + dateFormat.format(timesheet.getTimesheetPK().getDateDebut()));
 		
 	}
 
-	
+	}
 	public List<Mission> findAllMissionByEmployeJPQL(int employeId) {
 		return timesheetRepository.findAllMissionByEmployeJPQL(employeId);
 	}
--- src/test/java/tn/esprit/spring/entreprise/tests/EntreprisesTests.java
+++ src/test/java/tn/esprit/spring/entreprise/tests/EntreprisesTests.java
@@ -2,24 +2,54 @@ package tn.esprit.spring.entreprise.tests;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.controller.RestControlEntreprise;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
 
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.junit.jupiter.api.Test;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class EntreprisesTests {
 	private static final Logger l = Logger.getLogger(EntreprisesTests.class);
 	
+
+	private static Instant startedAt;
+	
 	@Autowired
 	RestControlEntreprise entrepriseControl;
 	
+	@BeforeClass	
+	  public static  void initStartingTime() {
+		l.info("Appel avant tous les tests");
+		startedAt = Instant.now();
+		
+	}
+	
+
+	@AfterClass	
+	 public static  void showTestDuration() {
+		l.info("Appel après tous les tests");
+		Instant endedAt = Instant.now();
+		long duration = Duration.between(startedAt, endedAt).toMillis();
+		// String ,long , on a passer a MessageFormat
+		l.info(MessageFormat.format("Durée des tests : {0} ms", duration));
+	}
+	 
+	 
 	@Test
 	public void testEntreprise() {
 		l.info("*********** Debut tests  **************");
@@ -59,7 +89,22 @@ Departement testDep = entrepriseControl.ajouterDepartement(depRH);
 	}
 
 
+/*
+	@Test
 
+	public void testGetEntrepriseById(){
+		Entreprise e= entrepriseControl.getEntrepriseById(89);
+		Assert.assertEquals(e.getId(),89);	
+	}
+	
+	
+	@Test
 
+	public void testDeleteEntrepriseById(){
+		entrepriseControl.deleteEntrepriseById(89);
+		Assert.assertNull(entrepriseControl.getEntrepriseById(89));
+	}
+
+*/
 
 }
